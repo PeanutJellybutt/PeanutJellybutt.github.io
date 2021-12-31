58cf3e400472f7931b8e5622b118e541b84e9878
PeanutJellyButt <woottipat@gmail.com>
2021-12-31 21:07:40 +0700

    2107

--- html5game/tph_extJs.js
+++ html5game/tph_extJs.js
@@ -1,43 +1,37 @@
 
-(function(extern, undefined){
+var audioContext;
+function getAudioContext() {
+	if (!audioContext) {
+		audioContext = new AudioContext;
+	}
+}
 
-		//
-    var X = 0;
-		var Y = 0;
+var X = 0;
+var Y = 0;
+function getMousePosX(w) {
+	return X * w;
+}
+function getMousePosY(h) {
+		return Y * h;
+}
 
-    extern['GetMousePosX'] = function(w) {
-        return X * w;
-    }
-		
-    extern['GetMousePosY'] = function(h) {
-        return Y * h;
-    }
-		
-		extern['GetMousePosX2'] = function(mX,mY,w,h,bw,bh) {
-        return mY + (bw - ((bw*16/9)/2));
-    }
-		
-    extern['GetMousePosY2'] = function(mX,mY,w,h,bw,bh) {
-        return bh-mX;
-    }
-		
-		function getCursorPosition(canvas, event) {
-				const rect = canvas.getBoundingClientRect();
-				X = (event.clientX - rect.left) / rect.width;
-				Y = (event.clientY - rect.top) / rect.height;
-		}
+function getMousePosX2(mX,mY,w,h,bw,bh) {
+		return mY + (bw - ((bw*16/9)/2));
+}
+function getMousePosY2(mX,mY,w,h,bw,bh) {
+		return bh-mX;
+}
 
-		const canvas = document.getElementById('canvas');
-		canvas.addEventListener('mousemove', function(e) {
-				getCursorPosition(canvas, e);
-		})
-		
-		//
-		var audioContext = false;
-		
-		extern['GetAudioContext'] = function() {
-        if !audioContext
-					audioContext = new AudioContext;
-    }
+function getCursorPosition(canvas, event) {
+		const rect = canvas.getBoundingClientRect();
+		X = (event.clientX - rect.left) / rect.width;
+		Y = (event.clientY - rect.top) / rect.height;
+}
 
-})(window['extJs'] = window['extJs']||{});
+var canvasHere;
+function CanvasSetListener() {
+	canvasHere = document.getElementById('canvas');
+	canvasHere.addEventListener('mousemove', function(e) {
+			getCursorPosition(canvasHere, e);
+	});
+}
