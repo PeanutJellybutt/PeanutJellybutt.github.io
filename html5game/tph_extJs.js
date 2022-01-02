d9b38eec69a873a95d4c4c7a561267009fc0def4
PeanutJellyButt <woottipat@gmail.com>
2022-01-02 11:53:44 +0700

    1153

--- html5game/tph_extJs.js
+++ html5game/tph_extJs.js
@@ -5,31 +5,50 @@ function getMousePosX(w) {
 	return X * w;
 }
 function getMousePosY(h) {
-		return Y * h;
+	return Y * h;
 }
 
 function getMousePosX2(mX,mY,w,h,bw,bh) {
-		return mY + (bw - ((bw*16/9)/2));
+	return mY + (bw - ((bw*16/9)/2));
 }
 function getMousePosY2(mX,mY,w,h,bw,bh) {
-		return bh-mX;
+	return bh-mX;
 }
 
 function getCursorPosition(canvas, event) {
-		const rect = canvas.getBoundingClientRect();
-		X = (event.clientX - rect.left) / rect.width;
-		Y = (event.clientY - rect.top) / rect.height;
+	const rect = canvas.getBoundingClientRect();
+	X = (event.clientX - rect.left) / rect.width;
+	Y = (event.clientY - rect.top) / rect.height;
 }
 
+var audioRecheck = false;
 var canvasHere;
-//var audioContext;
 function canvasSetListener() {
-	//if (audioContext == null) {
-	//	createAudioContext();
-	//}
 	canvasHere = document.getElementById('canvas');
 	canvasHere.addEventListener('mousemove', function(e) {
 			getCursorPosition(canvasHere, e);
+	});
+	document.addEventListener('visibilitychange', function(e) {
+			audioRecheck = true;
+			console.log("audioRecheck needed")
+	});
+	
+	var myEvent = ('ontouchstart' in document.documentElement) ? 'touchend' : 'click';
+	canvasHere.addEventListener(myEvent, function() {
+		if(typeof g_WebAudioContext == "undefined" || g_WebAudioContext == null) {
+			var AudioContext = window.AudioContext || window.webkitAudioContext;
+			g_WebAudioContext = new AudioContext;
+			g_WebAudioContext.resume();
+			console.log("NEW AUDIO");
+		} else if (audioRecheck) { 
+			g_WebAudioContext.suspend();
+			g_WebAudioContext.resume();
+			audioRecheck = false;
+			console.log("RERUN AUDIO");
+		} else if (g_WebAudioContext.state !== 'running') {
+			g_WebAudioContext.resume();
+			console.log("RESUME AUDIO");
+		}
 	});
 }
 
@@ -37,42 +56,11 @@ function browser_click_handler_init_js() {
 		console.log("INIT_HANDLER_JS");
 		canvasHere = document.getElementById('canvas');
     canvasHere.addEventListener("click", function() {
-			//
-			/*
-			if (!audioContext) {
-				createAudioContext();
-			}
-			console.log('click ' + audioContext.state);
-			if (audioContext.state !== 'running') {
-				audioContext.resume();
-			}
-			console.log('= ' + audioContext.state);
-			//
-			*/
 			gml_Script_gmcallback_browser_click_handler();
 		});
     return 0;
 }
-/*
-var resumeButton = null;
-function createAudioContext() {
-	audioContext = new AudioContext;
-	audioContext.resume();
-	audioContext.onstatechange = function() {
-		console.log('change: ' + audioContext.state);
-		if ((audioContext.state !== 'running') && (resumeButton == null)){
-			resumeButton = document.createElement("button");
-			resumeButton.innerHTML = "Resume?";
-			resumeButton.type = "button";
-			resumeButton.onclick = function () {
-				audioContext.resume();
-				resumeButton.remove();
-			};
-			document.body.appendChild(resumeButton);
-		}
-	}
-}
-*/
+
 function checkIfTouch() {
 	return (navigator.maxTouchPoints > 0);
 }
